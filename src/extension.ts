51847d0193c5a41ff03dee1390f40d81d77a29ea
Michael Stocker <michael.stocker@kwasi-ich.de>
2023-07-21 16:14:07 +0000

    * fix linter

--- src/extension.ts
+++ src/extension.ts
@@ -6,7 +6,7 @@ import { TerminalEnvironmentManager } from "./terminal";
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	const disposables = []
+	const disposables = [];
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "dotEnv" is now active!');
